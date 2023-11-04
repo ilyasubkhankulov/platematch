a47408b68762f4e7b64eef1af8a1b7b025dcf939
Allison Braithwaite <alliroseb@gmail.com>
2023-11-04 14:27:54 -0700

    helpful print

--- match-car.py
+++ match-car.py
@@ -28,19 +28,32 @@ class CarMatch:
         self.color_result = color_result
         self.overall_result = overall_result
         
+    def __str__(self):
+        return (f"CarMatch - \nOverall Result: {self.overall_result}, \n"
+                f"Make: {self.make_result.match_result},\n"
+                f"   Plate Value: {self.make_result.plate_value},\n"
+                f"   Car Value: {self.make_result.car_value},\n"
+                f"Model: {self.model_result.match_result},\n"
+                f"   Plate Value: {self.model_result.plate_value},\n"
+                f"   Car Value: {self.model_result.car_value},\n"
+                f"Year: {self.year_result.match_result},\n"
+                f"   Plate Value: {self.year_result.plate_value},\n"
+                f"   Car Value: {self.year_result.car_value},\n"
+                f"Color: {self.color_result.match_result}\n"
+                f"   Plate Value: {self.color_result.plate_value},\n"
+                f"   Car Value: {self.color_result.car_value}\n")
+        
 
 # based on the plate lookup response and the car lookup response, determine if the car matches the plate
 def match_car(plate, car) -> CarMatch:
-
-
-    make_result = ComparisonResult(compare_field(ComparisonValue.Make, plate['vin']['make'], car['vin']['make']), ComparisonValue.MAKE, plate['vin']['make'], car['car']['make'])
-    model_result = ComparisonResult(compare_field(ComparisonValue.Model, plate['vin']['model'], car['vin']['model']), ComparisonValue.MODEL, plate['vin']['model'], car['car']['model'])
-    year_result = ComparisonResult(compare_field(ComparisonValue.Year, plate['vin']['year'], car['car']['years']), ComparisonValue.YEAR, plate['vin']['year'], car['car']['years'])
+    make_result = ComparisonResult(compare_field(ComparisonValue.MAKE, plate['vin']['make'], car['car']['make']), ComparisonValue.MAKE, plate['vin']['make'], car['car']['make'])
+    model_result = ComparisonResult(compare_field(ComparisonValue.MODEL, plate['vin']['model'], car['car']['model']), ComparisonValue.MODEL, plate['vin']['model'], car['car']['model'])
+    year_result = ComparisonResult(compare_field(ComparisonValue.YEAR, plate['vin']['year'], car['car']['years']), ComparisonValue.YEAR, plate['vin']['year'], car['car']['years'])
     # If the car color probability is low, we can't be sure so return indeterminate
-    if car['color']['probability'] < 0.5 or plate['vin']['color'].abbreviation == 'UNK':
-        color_result = ComparisonResult(MatchResult.INDETERMINATE, ComparisonValue.COLOUR, plate['vin']['color'].name , car['color']['name'])
+    if car['color']['probability'] < 0.5 or plate['vin']['color']['abbreviation'] == 'UNK':
+        color_result = ComparisonResult(MatchResult.INDETERMINATE, ComparisonValue.COLOUR, plate['vin']['color']['name'] , car['color']['name'])
     else:
-        color_result = ComparisonResult(compare_field(ComparisonValue.COLOUR, plate['vin']['color'].name, car['color']['name']), ComparisonValue.COLOUR, plate['vin']['color'].name , car['color']['name'])
+        color_result = ComparisonResult(compare_field(ComparisonValue.COLOUR, plate['vin']['color']['name'], car['color']['name']), ComparisonValue.COLOUR, plate['vin']['color'].name , car['color']['name'])
         
     results = [make_result, model_result, year_result, color_result]
     # If any value is a mismatch, the overall result is a mismatch
@@ -89,45 +102,48 @@ def split_years(years_range):
 
 # Example Responses
 # plate lookup response
-# { 
-#  'success': True, 
-#  'vin': {
-#      'vin': '3TMMU4FN7FM086431', 
-#      'year': '2015', 
-#      'make': 'Toyota', 
-#      'model': 'Tacoma', 
-#      'trim': 'Delux Grade', 
-#      'name': '2015 Toyota Tacoma', 
-#      'engine': '4.0L V6 DOHC', 
-#      'style': 'PICKUP', 
-#      'transmission': '', 
-#      'driveType': '4WD/4-Wheel Drive/4x4', 
-#      'fuel': 'Gasoline', 
-#      'color': {'name': 'Unknown', 'abbreviation': 'UNK'}
-#      }
-#  }
+example_plate = { 
+ 'success': True, 
+ 'vin': {
+     'vin': '3TMMU4FN7FM086431', 
+     'year': '2015', 
+     'make': 'Toyota', 
+     'model': 'Tacoma', 
+     'trim': 'Delux Grade', 
+     'name': '2015 Toyota Tacoma', 
+     'engine': '4.0L V6 DOHC', 
+     'style': 'PICKUP', 
+     'transmission': '', 
+     'driveType': '4WD/4-Wheel Drive/4x4', 
+     'fuel': 'Gasoline', 
+     'color': {'name': 'Unknown', 'abbreviation': 'UNK'}
+     }
+ }
 
 # car lookup response
-# {
-#    "car":{
-#       "make":"Lexus",
-#       "model":"IS",
-#       "generation":"III facelift (2016-2020)",
-#       "years":"2016-2020",
-#       "prob":"100.00"
-#    },
-#    "color":{
-#       "name":"Gray",
-#       "probability":0.6417
-#    },
-#    "angle":{
-#       "name":"Back Left",
-#       "probability":1.0
-#    },
-#    "bbox":{
-#       "br_x":0.9915,
-#       "br_y":0.848,
-#       "tl_x":0.0,
-#       "tl_y":0.1845
-#    }
-# }
+example_car = {
+   "car":{
+      "make":"Lexus",
+      "model":"IS",
+      "generation":"III facelift (2016-2020)",
+      "years":"2016-2020",
+      "prob":"100.00"
+   },
+   "color":{
+      "name":"Gray",
+      "probability":0.6417
+   },
+   "angle":{
+      "name":"Back Left",
+      "probability":1.0
+   },
+   "bbox":{
+      "br_x":0.9915,
+      "br_y":0.848,
+      "tl_x":0.0,
+      "tl_y":0.1845
+   }
+}
+
+if __name__ == '__main__':
+    print(match_car(example_plate, example_car))
