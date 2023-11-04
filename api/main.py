e91721e406473a469fdfc2e95e99329666e9b37e
Ilya Subkhankulov <ilya.subkhankulov@gmail.com>
2023-11-04 14:48:44 -0700

    cleaned up api

--- api/main.py
+++ api/main.py
@@ -5,27 +5,38 @@ import aiofiles
 from fastapi import FastAPI, File, Form, UploadFile
 from pydantic import BaseModel
 
+TMP_DIR = 'objects'
+
 app = FastAPI()
 
 
 app.get("/")(lambda: {"Hello": "World"})
 
 
+async def save_to_disk(image, tmp_dir):
+    # save the file
+    path = os.path.join(tmp_dir, image.filename)
+    async with aiofiles.open(path, 'wb') as out_file:
+        content = await image.read()  # async read
+        await out_file.write(content)  # async write
+    return path
+
+
 class ImageMetadata(BaseModel):
     lat: str
     long: str
 
-@app.post("/upload/")
-async def upload_image(metadata: str = Form(...), image: UploadFile = File(...)):
-    metadata_dict = json.loads(metadata)
-    print(metadata_dict)
 
-    # save the file
-    path = os.path.join('objects', image.filename)
-    async with aiofiles.open(path, 'wb') as out_file:
-        content = await image.read()  # async read
-        await out_file.write(content)  # async write
+@app.post("/upload/")
+async def upload_image(
+        metadata: str = Form(...),
+        image: UploadFile = File(...)):
+   
+    metadata_dict = json.loads(metadata)
+    print("inputted metadata: ", metadata_dict)
+
+    path = await save_to_disk(image, TMP_DIR)
+    print("image saved to disk: ", path)
 
     return {"filename": image.filename,
-            "metadata": metadata_dict, 
-            "path": path}
+            "metadata": metadata_dict}
