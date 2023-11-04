3c2037cec72cb9069b9aea510775a4e9b0017755
Vadims Casecnikovs (Vadim Chashechnikov) <chashechnikovvadim@gmail.com>
2023-11-04 14:59:27 -0700

    convert image

---
+++ convert_image.py
@@ -0,0 +1,12 @@
+import io
+from PIL import Image
+from pillow_heif import register_heif_opener, HeifImage
+
+
+def heic_to_png_buffer(heic_path):
+    register_heif_opener()
+    heic_image = Image.open(heic_path)
+    png_buffer = io.BytesIO()
+    heic_image.save(png_buffer, format="PNG")
+    png_buffer.seek(0)
+    return png_buffer
